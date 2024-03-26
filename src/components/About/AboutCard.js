febee9ad4106d3d70d1c83a82cd98c7231e6cc37
hmedina24 <hectormedina@Hectors-MacBook-Air.local>
2024-03-26 17:38:30 -0400

    [Tuesday, March 26, 2024 17:38] Updated resume, modified \'About Me\' section, changed tab name, and added GitHub calendar to React portfolio.

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,33 +8,32 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
+            Hello world! I'm <span className="purple">Hector Medina </span>
+            based in <span className="purple"> Boston, Massachusetts, United States of America.</span>
             <br />
-            I am currently employed as a software developer at Juspay.
+            I'm a passionate software developer engineer with a solid background in React.JS, TypeScript, JavaScript, Java and Python.
             <br />
-            I have completed Integrated MSc (IMSc) in Maths and Computing at BIT
-            Mesra.
+            I earned my Bachelor's Degree from Suffolk University.
             <br />
             <br />
             Apart from coding, some other activities that I love to do!
           </p>
           <ul>
             <li className="about-activity">
-              <ImPointRight /> Playing Games
+              <ImPointRight /> Sharpen My DJ Skills 
             </li>
             <li className="about-activity">
-              <ImPointRight /> Writing Tech Blogs
+              <ImPointRight /> Stay Active: Work-out, Hike  
             </li>
             <li className="about-activity">
-              <ImPointRight /> Travelling
+              <ImPointRight /> Travel The World 
             </li>
           </ul>
 
           <p style={{ color: "rgb(155 126 172)" }}>
-            "Strive to build things that make a difference!"{" "}
+            "Always code as if the guy who ends up maintaining your code will be a violent psychopath who knows where you live."{" "}
           </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
+          <footer className="blockquote-footer">John Woods</footer>
         </blockquote>
       </Card.Body>
     </Card>
